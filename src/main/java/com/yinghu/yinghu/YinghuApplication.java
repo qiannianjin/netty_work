fffe649ac80441baae1611cbcb81d4ebe31a742d
qiannianjin <1691686144@qq.com>
2020-08-22 00:38:33 +0800

    初始化完成

---
+++ src/main/java/com/yinghu/yinghu/YinghuApplication.java
@@ -0,0 +1,13 @@
+package com.yinghu.yinghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinghuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YinghuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yinghu/yinghu/YinghuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yinghu.yinghu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YinghuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

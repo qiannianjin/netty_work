f29ee07951ad4ded35d9add5e729ffe0d4635761
qiannianjin <1691686144@qq.com>
2020-09-15 23:58:38 +0800

    2020-9-15

---
+++ src/main/java/com/yinghu/yinghu/nettyIo/EchoClientHandler.java
@@ -0,0 +1,43 @@
+package com.yinghu.yinghu.nettyIo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+import java.nio.ByteBuffer;
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+
+    public void channelActive(ChannelHandlerContext ctx){
+
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+
+    public void channelRead0(ChannelHandlerContext ctx,ByteBuf in){
+
+
+        System.out.println(
+                "Client recived:"+in.toString(CharsetUtil.UTF_8)
+        );
+
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause){
+        cause.printStackTrace();
+        ctx.close();
+
+    }
+
+
+    @Override
+    protected void messageReceived(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+
+    }
+}

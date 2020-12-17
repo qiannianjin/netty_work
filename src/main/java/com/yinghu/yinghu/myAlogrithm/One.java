992e274028eeb451717613b5ac40e5a0db9fd075
qiannianjin <1691686144@qq.com>
2020-12-17 21:29:42 +0800

    2020-12-17

--- src/main/java/com/yinghu/yinghu/myAlogrithm/One.java
+++ src/main/java/com/yinghu/yinghu/myAlogrithm/One.java
@@ -26,11 +26,48 @@ public class One {
     private static int partition(int[] a, int begin, int end) {
         //pivot:标杆的位置，counter：小于pivot的元素个数
         int pivot = end, counter = begin;
-        for(int i=begin; i<end; i++) {
-            if(a[i]<a[pivot]){
-                int temp=a[counter];
+        for (int i = begin; i < end; i++) {
+            if (a[i] < a[pivot]) {
+                int temp = a[counter];
+                a[counter] = a[i];
+                a[i] = temp;
+                counter++;
             }
         }
+        int temp = a[pivot];
+        a[pivot] = a[counter];
+        a[counter] = temp;
+        return counter;
+    }
+
+    //归并排序
+    public static void mergeSort(int[] array, int left, int right) {
+        if (right <= left) return;
+        int mid = (left + right) >> 1;//(left+right)/2
+
+        mergeSort(array, left, mid);
+        mergeSort(array, mid + 1, right);
+        merge(array, left, mid, right);
+
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];//中间数组
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j < right) {
+            temp[k++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
+        }
+        while (i < mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+        for (int p = 0; p < temp.length; p++) {
+            arr[left + p] = temp[p];
+        }
+    }
+
+    //堆排序
+    public static void heapSort(int[] array) {
+        if (array.length == 0) return;
 
 
     }
